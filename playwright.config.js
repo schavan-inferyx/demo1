ba20e1dfa6a1539b28d6680c594dabd50abd2631
schavan-inferyx <schavan@inferyx.com>
2025-08-29 15:23:20 +0530

    retry time decreased to 1

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   // workers: process.env.CI ? 1 : undefined,
   workers: 1,       // force single worker
   reporter: [["html"], ["list"], ["allure-playwright"]],
